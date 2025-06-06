59646639ee3e7e7635c5008ad932600d535ab331
Alex Courter <alex.courter@gmail.com>
2025-06-05 23:25:41 -0400

    build error fix

--- lmrk/src/app/api/users/[userId]/route.ts
+++ lmrk/src/app/api/users/[userId]/route.ts
@@ -3,10 +3,10 @@ import { getUsersCollection } from "@/models/User";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { userId: string } }
+  params: { params: { userId: string } }
 ) {
   const users = await getUsersCollection();
-  const user = await users.findOne({ userId: params.userId });
+  const user = await users.findOne({ userId: params.params.userId });
   if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }

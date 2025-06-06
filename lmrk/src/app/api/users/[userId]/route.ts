052a3da6b82d6981efcfb30f1a4a1d50f12c00b4
Alex Courter <alex.courter@gmail.com>
2025-06-05 23:59:19 -0400

    fix user route

--- lmrk/src/app/api/users/[userId]/route.ts
+++ lmrk/src/app/api/users/[userId]/route.ts
@@ -2,18 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUsersCollection } from "@/models/User";
 import { ObjectId } from "mongodb";
 
+type Params = { params: { userId: string } };
+
 export async function GET(
   req: NextRequest,
-  { params }: { params: { userId: string } }
+  context: Params
 ) {
+  const userId = context.params.userId;
   const users = await getUsersCollection();
   try {
-    const user = await users.findOne({ _id: new ObjectId(params.userId) });
+    const user = await users.findOne({ _id: new ObjectId(userId) });
     if (!user) {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
     return NextResponse.json(user);
-  } catch {
+  } catch (error) {
     return NextResponse.json({ error: "Invalid userId" }, { status: 400 });
   }
 }
